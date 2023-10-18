a7761ae9d8d3d15f88e1ec465278424dbc88e497
Jin_Y <wlsdud6221@naver.com>
2023-10-19 03:57:05 +0900

    API 적용 분리

--- src/components/common/InfoForm.jsx
+++ src/components/common/InfoForm.jsx
@@ -420,7 +420,6 @@ import styled from 'styled-components';
 import ButtonLong from '../common/ButtonLong';
 import { useState, useEffect } from 'react';
 // import A from '../common/A';
-import axios from 'axios';
 // import { useCookies } from 'react-cookie';
 
 const S = {
@@ -493,37 +492,17 @@ const InfoForm = ({ type, onSubmit }) => {
       }
     }
   };
-
   const btnSubmit = async (e) => {
     e.preventDefault();
     if (btnOn) {
-      try {
-        // Axios를 사용하여 PHP 스크립트에 데이터를 전송
-        const response = await axios.post(
-          'https://daishi7462.cafe24.com/php/loginProcess.php',
-          {
-            name: data.name,
-            email: data.id,
-            password: data.pw,
-            type: type,
-          },
-        );
-
-        // 서버에서의 응답 처리
-        if (response.data.success) {
-          // 성공적으로 처리되었을 때의 동작 (예: 페이지 이동 또는 메시지 표시)
-          console.log('로그인/가입 성공');
-          //세션값 받아오는 코드 작성.
-        } else {
-          // 실패 시 동작 (예: 에러 메시지 표시)
-          console.log('로그인/가입 실패');
-          console.log(response);
-          alert('로그인에 실패하였습니다! : ' + response.data.message);
-        }
-      } catch (error) {
-        console.error(error);
-        // 네트워크 에러 또는 서버 에러 처리
-      }
+      // {
+      //   if (type === 'register') {
+      //     console.log('회원가입경로');
+      //   } else {
+      //     LoginApi(data, type);
+      //   }
+      // }
+      onSubmit(data, type);
     }
   };
 
