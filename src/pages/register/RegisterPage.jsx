aabc30bb66420d9860fba20b34b338bc3da36f50
Jin_Y <wlsdud6221@naver.com>
2023-10-20 00:09:34 +0900

    RegisterApi 연동

--- src/pages/register/RegisterPage.jsx
+++ src/pages/register/RegisterPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import InfoForm from '../../components/common/InfoForm';
+import { RegisterApi } from '../../lib/apis/RegisterApi';
+import { useNavigate } from 'react-router-dom';
 
 const S = {
   content: styled.main`
@@ -9,9 +11,26 @@ const S = {
   `,
 };
 const RegisterPage = () => {
+  const navigate = useNavigate();
+
+  const handleRegister = async (inputs) => {
+    try {
+      const response = await RegisterApi(inputs);
+      if (response.success) {
+        console.log('회원가입 성공');
+        alert('환영합니다!');
+      } else {
+        console.log('회원가입 실패');
+        alert('회원가입에 실패하였습니다! : ' + response.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <S.content>
-      <InfoForm type="register" />
+      <InfoForm type="register" onSubmit={handleRegister} />
     </S.content>
   );
 };
