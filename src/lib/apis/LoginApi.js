6aa4ac90c3bd2c840094bea2a3095bd5b42fe968
Jin_Y <wlsdud6221@naver.com>
2023-10-22 02:17:28 +0900

    로그인 api 수정

--- src/lib/apis/LoginApi.js
+++ src/lib/apis/LoginApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const loginApi = async (inputs) => {
+export const LoginApi = async (inputs) => {
   try {
     const response = await axios.post(
       'https://daishi7462.cafe24.com/php/sign_in.php',
