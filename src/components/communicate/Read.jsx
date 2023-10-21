580e0b9e1ff841c97d10f2d0e2746c509b549ba6
Jin_Y <wlsdud6221@naver.com>
2023-10-22 02:19:29 +0900

    커뮤니티 게시글 상세읽기 Api 구현

--- src/components/communicate/Read.jsx
+++ src/components/communicate/Read.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import Button from '../common/Button';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { CommuReadApi } from '../../lib/apis/CommuReadApi';
 
 const S = {
   InputBox: styled.div`
@@ -24,7 +28,8 @@ const S = {
     margin: 10px auto;
   `,
   ButtonBox: styled.div`
-    width: auto;
+    display: flex;
+    width: 80px;
     height: auto;
     position: relative;
     text-align: right;
@@ -65,7 +70,7 @@ const S = {
   `,
   PwInput: styled.input`
     display: block;
-    width: 300px;
+    width: 200px;
     height: 32px;
     border: solid 1px #dadada;
     border-radius: 10px;
@@ -77,18 +82,40 @@ const S = {
 };
 
 const Read = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const idx = location.state.itemIdx;
+
+  const [itemData, setItemData] = useState({ title: '', content: '' });
+
+  useEffect(() => {
+    // 이펙트 함수를 통해 데이터를 가져옵니다.
+    GetCommuRead(idx);
+  }, []);
+
+  const GetCommuRead = async (idx) => {
+    try {
+      const response = await CommuReadApi(idx);
+      setItemData(response);
+    } catch (error) {
+      console.error('데이터 가져오기 실패:', error);
+    }
+  };
+
   return (
     <>
       <S.TitleInput
         type="text"
         maxlength="30"
         placeholder="제목을 입력하세요."
+        value={itemData.title}
         readOnly
       />
       <S.Text
         type="text"
         maxlength="1000"
         placeholder="내용을 입력하세요."
+        value={itemData.content}
         readOnly
       />
       <S.Box>
@@ -98,7 +125,15 @@ const Read = () => {
           placeholder="비밀번호를 입력하세요."
         />
         <S.ButtonBox>
-          <Button text={'수 정 하 기'} />
+          <Button text={'수 정'} />
+        </S.ButtonBox>
+        <S.ButtonBox>
+          <Button
+            text={'이 전'}
+            onClick={() => {
+              navigate('/commu');
+            }}
+          />
         </S.ButtonBox>
       </S.Box>
       <S.PN></S.PN>
