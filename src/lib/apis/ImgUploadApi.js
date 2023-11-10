e31d822e3b1da5a652e15618dea5241bb66d6b6b
Jin_Y <wlsdud6221@naver.com>
2023-11-11 01:41:25 +0900

    이미지 업로드 api url 변경문제 수정

--- src/lib/apis/ImgUploadApi.js
+++ src/lib/apis/ImgUploadApi.js
@@ -1,4 +1,26 @@
+// import axios from 'axios';
+
+// export const ImgUploadApi = async (file, parts) => {
+//   const formData = new FormData();
+//   formData.append('image', file);
+//   formData.append('parts', parts);
+//   console.log('보낼 데이터', formData);
+//   console.log('보낼 데이터', file);
+//   console.log('보낼 데이터', parts);
+//   try {
+//     const response = await axios.post(
+//       'http://43.202.47.103:5000/predict',
+//       formData,
+//     );
+//     console.log(response.data);
+//     return response.data;
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// };
 import axios from 'axios';
+import { instance } from './instance';
 
 export const ImgUploadApi = async (file, parts) => {
   const formData = new FormData();
@@ -8,10 +30,7 @@ export const ImgUploadApi = async (file, parts) => {
   console.log('보낼 데이터', file);
   console.log('보낼 데이터', parts);
   try {
-    const response = await axios.post(
-      'http://43.202.47.103:5000/predict',
-      formData,
-    );
+    const response = await instance.post('/predict', formData);
     console.log(response.data);
     return response.data;
   } catch (error) {
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/predict', // 불러오려는 server 의 api path
-    createProxyMiddleware({
+    createProxyMiddleware('/predict', {
       target: 'http://43.202.47.103:5000', // server 주소를 넣어주면 된다.
       changeOrigin: true,
     }),
