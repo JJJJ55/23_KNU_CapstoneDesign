070a1f6cee4b01c5cc1e1e438b2aaf71f200035c
Jin_Y <wlsdud6221@naver.com>
2023-11-16 00:18:53 +0900

    딥러닝 Api url 변경

--- src/lib/apis/instance.js
+++ src/lib/apis/instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://3.37.37.124:5000',
+  baseURL: 'https://www.repairproject.net',
 });
 
 export { instance };
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/predict', {
-      target: 'http://3.37.37.124:5000', // server 주소를 넣어주면 된다.
+      target: 'https://www.repairproject.net', // server 주소를 넣어주면 된다.
       changeOrigin: true,
     }),
   );
