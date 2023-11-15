3c5bca38575a25202961b8d1b62b685dc42f27ea
Jin_Y <wlsdud6221@naver.com>
2023-11-16 00:48:26 +0900

    cors

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware('/predict', {
+    '/predict',
+    createProxyMiddleware({
       target: 'https://www.repairproject.net', // server 주소를 넣어주면 된다.
       changeOrigin: true,
     }),
