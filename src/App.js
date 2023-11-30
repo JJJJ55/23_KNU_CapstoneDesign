de3dd5ca41e3186cf021bbde9e849e61bc39f1bf
Jin_Y <wlsdud6221@naver.com>
2023-11-30 23:26:25 +0900

    앱 설치유도 페이지 작동

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ function App() {
       <S.Frame>
         <BrowserRouter basename={process.env.PUBLIC_URL}>
           <Routes>
-            {true ? (
+            {isStandalone ? (
               <>
                 {userLogin ? (
                   <>
@@ -108,98 +108,3 @@ function App() {
   );
 }
 export default App;
-
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import styled from 'styled-components';
-// import HomePage from './pages/HomePage.jsx';
-// import GlobalStyle from './style/GlobalStyle.js';
-// import RegisterPage from './pages/register/RegisterPage.jsx';
-// import LoginPage from './pages/Login/LoginPage.jsx';
-// import LodingPage from './pages/LodingPage.jsx';
-// import MainPage from './pages/Main/MainPage.jsx';
-// import IntroPage from './pages/Intro/IntroPage.jsx';
-// import CommuPage from './pages/Communicate/CommuPage.jsx';
-// import ReadPage from './pages/Communicate/ReadPage.jsx';
-// import WritePage from './pages/Communicate/WritePage.jsx';
-// import CarPage from './pages/CarDamage/CarPage.jsx';
-// import MapPage from './pages/CarMap/MapPage.jsx';
-// import ErrorPage from './pages/ErrorPage.jsx';
-// import { useContext, useEffect } from 'react';
-// import { LoginStateContext } from './lib/context/LoginContext.jsx';
-// import ModifyPage from './pages/Communicate/ModifyPage.jsx';
-// import bg from './assets/img/RepairBg.jpg';
-// import YourComponent from './components/Modal/YourComponent.jsx';
-// import ChangePasswordPage from './pages/Login/ChagePasswordPage.jsx';
-
-// // 애플리케이션 진입점에서 경고 레벨 조절
-// console.error = (message) => {};
-// console.warn = (message) => {};
-
-// const S = {
-//   Frame: styled.div`
-//     max-width: 390px;
-//     height: 100svh;
-//     display: flex;
-//     position: relative;
-//     margin: 0 auto;
-//     background-color: #b8e0fe;
-//     box-shadow: -5px -5px 30px 5px black;
-//     flex-direction: column;
-//   `,
-
-//   Background: styled.div`
-//     width: 100vw;
-//     height: 100%;
-//     background: #f7f7f7;
-//     background-image: url(${bg});
-//     background-position: center center;
-//     background-repeat: no-repeat;
-//     background-size: cover;
-//     position: absolute;
-//     font-size: 80px;
-//     line-height: 70px;
-//     font-family: 'Racing Sans One';
-//   `,
-// };
-
-// function App() {
-//   const userLogin = useContext(LoginStateContext);
-
-//   return (
-//     <>
-//       <GlobalStyle />
-//       <S.Background />
-//       <S.Frame>
-//         <BrowserRouter basename={process.env.PUBLIC_URL}>
-//           <Routes>
-//             {userLogin ? (
-//               <>
-//                 <Route path="/" element={<LodingPage />} />
-//                 <Route path="/main" element={<MainPage />} />
-//                 <Route path="/intro" element={<IntroPage />} />
-//                 <Route path="/commu" element={<CommuPage />} />
-//                 <Route path="commu/read" element={<ReadPage />} />
-//                 <Route path="commu/write" element={<WritePage />} />
-//                 <Route path="commu/modify" element={<ModifyPage />} />
-//                 <Route path="/car" element={<CarPage />} />
-//                 <Route path="/map" element={<MapPage />} />
-//                 <Route path="/change" element={<ChangePasswordPage />} />
-//                 <Route path="/*" element={<ErrorPage />} />
-//                 <Route path="/test" element={<YourComponent />} />
-//               </>
-//             ) : (
-//               <>
-//                 <Route path="/" element={<LodingPage />} />
-//                 <Route path="/home" element={<HomePage />} />
-//                 <Route path="/register" element={<RegisterPage />} />
-//                 <Route path="/login" element={<LoginPage />} />
-//                 <Route path="/*" element={<ErrorPage />} />
-//               </>
-//             )}
-//           </Routes>
-//         </BrowserRouter>
-//       </S.Frame>
-//     </>
-//   );
-// }
-// export default App;
